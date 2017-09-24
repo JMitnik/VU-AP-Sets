4389bd1555cc55d94ba39482172986562625b71f
Jonathan Mitnik <JMitnik@gmail.com>
2017-09-24 19:09:48 +0200

    Set fixed constructions

--- src/main/java/Set.java
+++ src/main/java/Set.java
@@ -3,10 +3,11 @@ import java.math.BigInteger;
 /**
  * @structure - Linear
  * @elements - Elements of type E
+ * TODO: Should this domain fit in here?
  * @domain - All natural numbers
  * @pre -
  * @post - A new empty Set has been created to be filled with elements E
- * @constructor - Set()
+ * @constructor - Set(List)
  */
 interface Set<E> {
     /**
