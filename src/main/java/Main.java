61ed8509b3563df96af9d11311cebb04dcbb5fa3
Jonathan Mitnik <JMitnik@gmail.com>
2017-10-02 15:30:54 +0200

    Set Specs Improvement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,12 @@
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class Main {
 
     private void start() {
         // Create a scanner on System.in
-        HashMap<String, int[]> test = new HashMap<>();
-        int[] arrayA = new int[] {4,5,6,7};
-        int[] arrayB = new int[] {4,5,6,7};
-
-        test.put("a", arrayA);
-        test.put("b", arrayB);
-        int i;
+        Scanner input = new Scanner(System.in);
     }
 
     public static void main(String[] argv) {
--- src/main/java/Set.java
+++ src/main/java/Set.java
@@ -3,38 +3,67 @@ import java.math.BigInteger;
 /**
  * @structure - Linear
  * @elements - Elements of type E
- * TODO: Should this domain fit in here?
- * @domain - All natural numbers
+ * @domain - Unique elements of Type E
  * @pre -
  * @post - A new empty Set has been created to be filled with elements E
- * @constructor - Set(List)
+ * @constructor - Set()
  */
-interface Set<E> {
+interface Set<E extends Comparable<E>> {
     /**
      * @param set
      * @pre -
      * @post -
      */
-    Set<E> union(Set set);
+    Set<E> union(Set<E> set);
+
+    /**
+     * Adds an element 'E' to the set.
+     * @param el
+     * @pre -
+     * @post - The element E has been added to the set at a random position.
+     */
+    void add(E el);
+
+    /**
+     * Removes a random element from the set.
+     * @pre - The set is not empty.
+     * @post - The set is returned without a random element.
+     */
+    void remove();
+
+    /**
+     * Gets random element E from the set.
+     * @pre - The set is not empty.
+     * @post - An element
+     */
+    E getRandomElement();
+
+    /**
+     * Returns the number of elements in the set.
+     * @pre -
+     * @post - The number of elements in the set has been returned.
+     * @return
+     */
+    int cardinality();
 
     /**
      * @param set
      * @pre -
      * @post -
      */
-    Set<E> intersection(Set set);
+    Set<E> intersection(Set<E> set);
 
     /**
      * @param set
      * @pre -
      * @post -
      */
-    Set<E> complement(Set set);
+    Set<E> complement(Set<E> set);
 
     /**
      * @param set
      * @pre -
      * @post -
      */
-    Set<E> symmDifference(Set set);
+    Set<E> symmDifference(Set<E> set);
 }
