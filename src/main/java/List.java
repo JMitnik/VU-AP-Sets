fd3a0358b21de0540a8aa7c27b4cd7c9da3f1a6b
Jonathan Mitnik <JMitnik@gmail.com>
2017-10-02 15:30:18 +0200

    List Implementation

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,4 +1,6 @@
 public class List<E extends Comparable> implements ListInterface<E>{
+    Node head;
+    Node current;
 
     private class Node {
 
@@ -11,15 +13,21 @@ public class List<E extends Comparable> implements ListInterface<E>{
         }
 
         public Node(E data, Node prior, Node next) {
+            //TODO:
             this.data = data == null ? null : data;
             this.prior = prior;
             this.next = next;
         }
     }
 
+    List() {
+        Node head = null;
+        Node current = head;
+    }
+
     @Override
     public boolean isEmpty() {
-        return false;
+        return ;
     }
 
     @Override
@@ -29,11 +37,24 @@ public class List<E extends Comparable> implements ListInterface<E>{
 
     @Override
     public int size() {
-        return 0;
+        return countNodes(head);
+    }
+
+    /**
+     *
+     */
+    private int countNodes(Node currentNode) {
+        if (currentNode == null) {
+            return 0;
+        }
+
+        return 1 + countNodes(currentNode.next);
     }
 
     @Override
     public ListInterface<E> insert(E d) {
+        Node currentNode = head;
+
         return null;
     }
 
@@ -54,21 +75,47 @@ public class List<E extends Comparable> implements ListInterface<E>{
 
     @Override
     public boolean goToFirst() {
-        return false;
+        if (size() == 0) {
+            return false;
+        }
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return true;
     }
 
     @Override
     public boolean goToLast() {
-        return false;
+        if (size() == 0) {
+            return false;
+        }
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return true;
     }
 
     @Override
     public boolean goToNext() {
+        if (current.next != null) {
+            current = current.next;
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean goToPrevious() {
+        if (current.prior != null) {
+            current = current.prior;
+            return true;
+        }
+
         return false;
     }
 
