240808fefc8b71a905d2366cf9f1a6befdcbdd51
Jonathan Mitnik <JMitnik@gmail.com>
2017-10-02 15:30:38 +0200

    Identifier Specs

---
+++ src/main/java/IdentiferImp.java
@@ -0,0 +1,17 @@
+public class IdentiferImp implements Identifier {
+//    char[] identifierCharacters;
+
+    @Override
+    public char[] getIdentifier() {
+        return new char[0];
+    }
+
+    @Override
+    public void addChar(char character) {
+    }
+
+    @Override
+    public void removeChar(int index) {
+
+    }
+}
--- src/main/java/Identifier.java
+++ src/main/java/Identifier.java
@@ -1,20 +1,34 @@
 /**
- *  @elements : -
- *	@structure : -
- *	@domain : 	-
- *	@constructor - Identifier();
+ *  @elements : Characters
+ *	@structure : Linear
+ *	@domain : Letters and numbers, where the first character is a letter.
+ *	@constructor - Identifier(char);
  *	<dl>
  *		<dt><b>PRE</b><dd>		- The Identifier
- *		<dt><b>POST</b><dd> 	A new Identifier has been created with the given string from the constructor.
+ *		<dt><b>POST</b><dd>  - A new empty Identifier has been created with the first char.
  * </dl>
  **/
 interface Identifier {
     /**
-     * Get the String value of the Identifier's name
+     * Get the full value of the Identifier's name
      * @pre -
-     * @post - The name of the identifier has been returned as a String.
+     * @post - The full character representation of the identifier has been returned as char[].
      */
-    String getIdentifierName();
+    char[] getIdentifier();
+
+    /**
+     * Adds a character to the identifier.
+     * @pre -
+     * @post -
+     */
+    void addChar(char character);
+
+    /**
+     * Removes the element at index 'index' of the char[].
+     * @pre -
+     * @post -
+     */
+    void removeChar(int index);
 
     /**
      * @pre -
