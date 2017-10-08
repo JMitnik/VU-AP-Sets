4bd34f326326967e300cffd3b3db857c3ced93b3
Jonathan Mitnik <JMitnik@gmail.com>
2017-10-08 23:59:49 +0200

    Identifiers

--- src/main/java/IdentiferImp.java
+++ src/main/java/IdentiferImp.java
@@ -1,18 +1,23 @@
 public class IdentiferImp implements Identifier {
-//    char[] identifierCharacters;
+    private StringBuffer identifierCharacters;
 
     @Override
     public String getIdentifier() {
-        return null;
+        return identifierCharacters.toString();
     }
 
     @Override
     public void addChar(char character) {
-
+        identifierCharacters.append(character);
     }
 
     @Override
     public void removeChar(int index) {
+        identifierCharacters.deleteCharAt(index);
+    }
 
+    @Override
+    public int hashCode() {
+        return getIdentifier().hashCode();
     }
 }
