3d130279f4edfc6b87b67f36bde0f981b03f950c
Jonathan Mitnik <JMitnik@gmail.com>
2017-10-12 18:58:31 +0200

    Set improvements

--- src/main/java/SetImp.java
+++ src/main/java/SetImp.java
@@ -3,11 +3,7 @@ import java.math.BigInteger;
 public class SetImp<E extends Comparable> implements Set<E>{
     private ListInterface<E> elements;
 
-    //TODO: Sets
-    //TODO - Can we copy a set by using an extra constructor?
-
     SetImp() {
-        //todo: Usage of List: do I instantiate it as a property, extend the List, or instantiate it in the program and give it to the constructor?
         this.elements = new List<E>();
     }
 
@@ -17,7 +13,7 @@ public class SetImp<E extends Comparable> implements Set<E>{
 
     @Override
     public void addEl(E el) {
-        if (!find(el)) { //TODO: Assumes we can use the list implementation and find property?
+        if (!find(el)) {
             elements.insert(el);
         }
     }
@@ -36,23 +32,13 @@ public class SetImp<E extends Comparable> implements Set<E>{
     }
 
     @Override
-    public boolean find(E matchEl) { //todo: If we can't use the find method of List, we do it this way.
-        Set<E> workingSet = this.copy();
-
-        while (workingSet.cardinality() != 0) {
-            E retrievedEl = workingSet.remove(); //todo: again casting works, why?
-
-            if (retrievedEl.equals(matchEl)) {
-                return true;
-            }
-        }
-
-        return false;
+    public boolean find(E matchEl) {
+        return this.elements.find(matchEl);
     }
 
     @Override
     public Set intersection(Set set) {
-        Set<E> result = new SetImp(); //todo: do we find a way to instantiate a new 'Interface'
+        Set<E> result = new SetImp();
         Set<E> workingSetLhs = this.copy();
         Set<E> workingSetRhs = set.copy();
 
@@ -112,6 +98,6 @@ public class SetImp<E extends Comparable> implements Set<E>{
 
     @Override
     public Set<E> copy() {
-        return new SetImp(elements.copy()); //todo: Copy with instance?
+        return new SetImp<>(this);
     }
 }
